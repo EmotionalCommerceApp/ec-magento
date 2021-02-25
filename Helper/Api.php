9d93fd449d3d338c2bdfa15ae6a659bdbe9dd82b
Charbel Wakim <charbel.wakim@keeward.com>
2021-02-25 21:14:55 +0100

    Create the function to upload a video

--- Helper/Api.php
+++ Helper/Api.php
@@ -8,6 +8,18 @@ class Api extends AbstractHelper
 
     const API_URL = 'localhost:8000';
 
+    /**
+     * ConfigFactory
+     *
+     * @var \Ec\Qr\Model\ConfigFactory
+     */
+    protected $configFactory;
+
+    public function __construct(
+        \Ec\Qr\Model\ConfigFactory $configFactory
+    ) {
+        $this->configFactory = $configFactory;
+    }
 
     public function getCampaigns($key, $secret, $domain)
     {
@@ -71,6 +83,75 @@ class Api extends AbstractHelper
         return [
             'success' => true,
             'data' => $campaigns,
+        ];
+
+    }
+
+    public function getConfig()
+    {
+        $configFactory = $this->configFactory->create();
+        $collection = $configFactory->getCollection();
+
+        $configData = [
+            'key' => false,
+            'secret' => false,
+            'domain' => false,
+            'campaign' => false,
+            'template' => false,
+            'price' => false,
+        ];
+
+        foreach ($collection as $config) {
+            $configData[$config->getName()] = $config->getValue();
+        }
+
+        return $configData;
+    }
+
+    public function uploadVideo($filePath)
+    {
+        $config = $this->getConfig();
+
+        $headers =  array(
+            'Accept: application/json',
+            'content-type: multipart/form-data',
+            'key: '.$config['key'],
+            'secret: '.$config['secret'],
+        );
+
+        $regUrl = $config['domain']. '.' . self::API_URL."/api/campaigns/video";
+
+         $data = [
+            'campaign' => $config['campaign'],
+            'file' => curl_file_create($filePath),
+        ];
+
+        $regCurl = curl_init($regUrl);
+        curl_setopt($regCurl, CURLOPT_POST, 1);
+        curl_setopt($regCurl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($regCurl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt(
+            $regCurl,
+            CURLOPT_HTTPHEADER,
+            $headers
+        );
+        $regResult = json_decode(curl_exec($regCurl));
+        $statusCode = curl_getinfo($regCurl, CURLINFO_HTTP_CODE);
+        curl_close($regCurl);
+
+        if ($statusCode != 200) {
+            return [
+                'success' => false,
+                'message' => $regResult->message,
+            ];
+        }
+
+        return [
+            'success' => true,
+            'data' => [
+                'url' => $regResult->url,
+                'qr' => $regResult->qr,
+            ],
         ];
 
     }
