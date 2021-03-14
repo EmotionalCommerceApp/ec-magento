68f219816e154d10e027a5ac910b9825f5641379
Charbel Wakim <charbel.wakim@keeward.com>
2021-03-14 15:25:18 +0100

    Add error message on ec product update

--- Observer/CartAddItem.php
+++ Observer/CartAddItem.php
@@ -10,15 +10,18 @@ class CartAddItem implements ObserverInterface
     protected $checkoutSession;
     protected $ecOrder;
     protected $apiHelper;
+    protected $messageManager;
 
     public function __construct(
         \Magento\Checkout\Model\Session $checkoutSession,
         \Ec\Qr\Model\EcOrderFactory $ecOrder,
-        \Ec\Qr\Helper\Api $apiHelper
+        \Ec\Qr\Helper\Api $apiHelper,
+        \Magento\Framework\Message\ManagerInterface $messageManager
     ){
         $this->checkoutSession = $checkoutSession;
         $this->ecOrder = $ecOrder;
         $this->apiHelper = $apiHelper;
+        $this->messageManager = $messageManager;
     }
 
 
@@ -28,6 +31,7 @@ class CartAddItem implements ObserverInterface
         if ($item->getSku() === 'ec-qr-product' && $item->getQty() > 1) {
             $item->setQty(1);
             $item->save();
+            $this->messageManager->addErrorMessage(__('You cannot update the quantity for this product'));
         }
     }
 }
