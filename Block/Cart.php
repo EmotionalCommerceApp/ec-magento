2b94f6378d926b3a80a9607ca01c5a51898d800e
Charbel Wakim <charbel.wakim@keeward.com>
2021-04-27 13:37:44 +0200

    Do not shoe the btton if the cart is empty

--- Block/Cart.php
+++ Block/Cart.php
@@ -12,21 +12,25 @@ class Cart extends \Magento\Framework\View\Element\Template
      */
     protected $apiHelper;
     protected $checkoutSession;
+    protected $cartHelper;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Ec\Qr\Helper\Api $apiHelper,
         \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Checkout\Helper\Cart $cartHelper,
         array $data = []
     ) {
         $this->apiHelper = $apiHelper;
         $this->checkoutSession = $checkoutSession;
+        $this->cartHelper = $cartHelper;
+
         parent::__construct($context, $data);
     }
 
     public function canShow()
     {
-        if ($this->checkoutSession->getData('ec_qr')) {
+        if ($this->checkoutSession->getData('ec_qr') || $this->cartHelper->getItemsCount() === 0) {
             return false;
         }
 
